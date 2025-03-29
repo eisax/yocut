2cdf08989fcccbbab07f7c749d1d2f75d2b134af
Josphat Kuda Ndhlovu <josphatndhlovu@Josphats-MacBook-Pro.local>
2025-03-29 12:22:39 +0200

    feat:update 3

--- lib/cubits/appConfigurationCubit.dart
+++ lib/cubits/appConfigurationCubit.dart
@@ -1,6 +1,9 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'dart:io';
+
 import 'package:yocut/data/models/appConfiguration.dart';
-import 'package:yocut/data/repositories/systemRepository.dart';
+import 'package:yocut/data/repositories/systemInfoRepository.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 abstract class AppConfigurationState {}
 
@@ -24,24 +27,16 @@ class AppConfigurationCubit extends Cubit<AppConfigurationState> {
   final SystemRepository _systemRepository;
 
   AppConfigurationCubit(this._systemRepository)
-    : super(AppConfigurationInitial());
+      : super(AppConfigurationInitial());
 
   Future<void> fetchAppConfiguration() async {
     emit(AppConfigurationFetchInProgress());
     try {
-      emit(
-        AppConfigurationFetchSuccess(
-          appConfiguration: AppConfiguration(
-            appLink: "null",
-            iosAppLink: "null",
-            appVersion: "null",
-            iosAppVersion: "null",
-            forceAppUpdate: "null",
-            appMaintenance: "null",
-            fileUploadSizeLimit: "null",
-          ),
-        ),
+      final appConfiguration = AppConfiguration.fromJson(
+        await _systemRepository.fetchSettings(type: "app_settings") ?? {},
       );
+
+      emit(AppConfigurationFetchSuccess(appConfiguration: appConfiguration));
     } catch (e) {
       emit(AppConfigurationFetchFailure(e.toString()));
     }
@@ -52,5 +47,44 @@ class AppConfigurationCubit extends Cubit<AppConfigurationState> {
       return (state as AppConfigurationFetchSuccess).appConfiguration;
     }
     return AppConfiguration.fromJson({});
+  }
+
+  String getAppLink() {
+    if (state is AppConfigurationFetchSuccess) {
+      return Platform.isIOS
+          ? getAppConfiguration().iosAppLink
+          : getAppConfiguration().appLink;
+    }
+    return "";
+  }
+
+  String getAppVersion() {
+    if (state is AppConfigurationFetchSuccess) {
+      return Platform.isIOS
+          ? getAppConfiguration().iosAppVersion
+          : getAppConfiguration().appVersion;
+    }
+    return "";
+  }
+
+  bool appUnderMaintenance() {
+    if (state is AppConfigurationFetchSuccess) {
+      return getAppConfiguration().appMaintenance == "1";
+    }
+    return false;
+  }
+
+  bool forceUpdate() {
+    if (state is AppConfigurationFetchSuccess) {
+      return getAppConfiguration().forceAppUpdate == "1";
+    }
+    return false;
+  }
+
+  String getFileSizeLimit() {
+    if (state is AppConfigurationFetchSuccess) {
+      return getAppConfiguration().fileUploadSizeLimit;
+    }
+    return "";
   }
 }
