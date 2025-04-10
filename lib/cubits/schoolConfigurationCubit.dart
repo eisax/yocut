7d22853f1fbf2fad7c51136863dc624842a11712
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-10 05:28:45 +0200

    Kudah's commit 2025-04-10 05:28:45

--- lib/cubits/schoolConfigurationCubit.dart
+++ lib/cubits/schoolConfigurationCubit.dart
@@ -53,7 +53,7 @@ class SchoolConfigurationCubit extends Cubit<SchoolConfigurationState> {
 
   Student getSchoolConfiguration() {
     if (state is SchoolConfigurationFetchSuccess) {
-      print(jsonEncode((state as SchoolConfigurationFetchSuccess).schoolConfiguration.body.profile));
+      print(jsonEncode((state as SchoolConfigurationFetchSuccess).schoolConfiguration.body));
       print("---------------test 5");
       return (state as SchoolConfigurationFetchSuccess).schoolConfiguration;
     }
