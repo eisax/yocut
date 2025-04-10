f804b6a58176ad846fa466cf7d13854739604717
Josphat Kuda Ndhlovu <josphatndhlovu@Josphats-MacBook-Pro.local>
2025-04-10 03:48:18 +0200

    Auto commit 2025-04-10 03:48:18

--- lib/cubits/schoolConfigurationCubit.dart
+++ lib/cubits/schoolConfigurationCubit.dart
@@ -38,7 +38,13 @@ class SchoolConfigurationCubit extends Cubit<SchoolConfigurationState> {
     try {
       emit(
         SchoolConfigurationFetchSuccess(
-          schoolConfiguration: SchoolConfiguration(sessionYear: SessionYear(), semesterDetails: SemesterDetails(), schoolSettings: SchoolSettings(), enabledPaymentGateways: [], enabledModules: {})
+          schoolConfiguration: SchoolConfiguration(
+            sessionYear: SessionYear(),
+            semesterDetails: SemesterDetails(),
+            schoolSettings: SchoolSettings(),
+            enabledPaymentGateways: [],
+            enabledModules: {},
+          ),
         ),
       );
     } catch (e) {
