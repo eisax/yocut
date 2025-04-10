d5c82458667008263274dd9f26dcfc0fb050521f
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-10 04:51:06 +0200

    Kudah's commit 2025-04-10 04:51:06

--- lib/data/repositories/schoolRepository.dart
+++ lib/data/repositories/schoolRepository.dart
@@ -6,6 +6,58 @@ import 'package:yocut/data/models/sliderDetails.dart';
 import 'package:yocut/utils/api.dart';
 
 class SchoolRepository {
+  String getJwtToken() {
+    return Hive.box(authBoxKey).get(jwtTokenKey) ?? "";
+  }
+
+  String getRegNumber() {
+    return Hive.box(authBoxKey).get(studentRegNumberKey) ?? "";
+  }
+
+  Future<Map<String, dynamic>> fetchSchoolDetails() async {
+    final Dio dio = Dio(
+      BaseOptions(
+        connectTimeout: Duration(seconds: 30),
+        receiveTimeout: Duration(seconds: 30),
+        followRedirects: false,
+        validateStatus: (status) => status != null && status < 400,
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Accept': 'application/json',
+          'User-Agent': 'Mozilla/5.0 (compatible; DartApp/1.0)',
+        },
+      ),
+    );
+
+    // final result = await Api.get(
+    //   url: '${Api.schoolDetails}/${getRegNumber()}/${getJwtToken()}',
+    //   useAuthToken: false,
+    // );
+
+    try {
+      final response = await dio.get(
+        '${Api.studentData}/${getRegNumber()}/${getJwtToken()}',
+      );
+
+      if (response.data == null) {
+        throw ApiException('API returned null response');
+      }
+
+      // Check if response data is a Map<String, dynamic>
+      if (response.data is Map<String, dynamic>) {
+        return {"student": Student.fromJson(response.data)};
+      }
+      // If it's a String, decode it into a Map
+      else if (response.data is String) {
+        final Map<String, dynamic> json = jsonDecode(response.data);
+        return {"student": Student.fromJson(json)};
+      } else {
+        throw ApiException('Unexpected response format');
+      }
+    } catch (e) {
+      throw ApiException('Failed to fetch student data: ${e.toString()}');
+    }
+  }
   Future<Student> getSchoolSchoolSettingDetails(
       {required bool useParentApi, int? childId}) async {
     try {
