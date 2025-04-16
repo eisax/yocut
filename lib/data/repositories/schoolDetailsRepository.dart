15d1406157cf745d9bbb1eefa16764ea47ef2e6c
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-16 12:00:00 +0200

    feat: update commit, please review commit

--- lib/data/repositories/schoolDetailsRepository.dart
+++ lib/data/repositories/schoolDetailsRepository.dart
@@ -52,7 +52,7 @@ class SchooldetailsfetchRepository {
       // If it's a String, decode it into a Map
       else if (response.data is String) {
         final Map<String, dynamic> json = jsonDecode(response.data);
-      pri
+      print(json);
         return {"student": Student.fromJson(json)};
       } else {
         throw ApiException('Unexpected response format');
