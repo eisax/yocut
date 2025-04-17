c550e66039e72584cb06c9cd64e5e4fdf162beea
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-17 11:55:19 +0200

    feat:update commit, please review  commit 2025-04-17 11:55:19

--- lib/ui/screens/exam/examTimeTableScreen.dart
+++ lib/ui/screens/exam/examTimeTableScreen.dart
@@ -77,7 +77,7 @@ class _ExamTimeTableState extends State<ExamTimeTableScreen> {
                   showShadow: true,
                   height: boxConstraints.maxWidth * (0.235),
                   radius: 10,
-                  subject: Module(moduleName:"", moduleId: "", moduleCode: "", moduleUnitCode: "", periodId: "", isEvaluable: "", pastExamPapers: [], vleStatus: true),
+                  subject:,
                   width: boxConstraints.maxWidth * (0.26),
                 ),
               ),
