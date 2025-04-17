ebbd7f49a135b0261d93a984710af56fbf7e28bb
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-17 12:29:13 +0200

    feat:update commit, please review  commit 2025-04-17 12:29:13

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -428,11 +428,12 @@ class Utils {
         .getSchoolConfiguration()
         .body.profile.firstName;
 
-        print("------module selected");
+       
 
     //Module id will have "1" or "1#2".
     final ids = moduleId.split(moduleIdJoiner).toList();
     if (ids.contains(defaultModuleId.toString())) {
+       print("------module selected");
       return true;
     }
 
