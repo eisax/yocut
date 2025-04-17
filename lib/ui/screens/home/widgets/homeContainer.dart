549977b9c4df3c636b467758c6c6d83f7b15fbb9
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-17 11:42:56 +0200

    feat:update commit, please review  commit 2025-04-17 11:42:56

--- lib/ui/screens/home/widgets/homeContainer.dart
+++ lib/ui/screens/home/widgets/homeContainer.dart
@@ -66,7 +66,7 @@ class _HomeContainerState extends State<HomeContainer> {
         SchoolConfigurationState>(
       listener: (context, state) {
         if (state is SchoolConfigurationFetchSuccess) {
-          if (state.schoolConfiguration. &&
+          if (state.schoolConfiguration.body.registration. &&
               state.electiveSubjects.isEmpty) {
             if (Get.currentRoute == Routes.selectSubjects) {
               return;
