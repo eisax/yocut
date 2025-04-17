4022c6d73b78d6de98c219a64423cc4e9b8ab82e
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-17 11:50:09 +0200

    feat:update commit, please review  commit 2025-04-17 11:50:09

--- lib/ui/widgets/subjectImageContainer.dart
+++ lib/ui/widgets/subjectImageContainer.dart
@@ -1,5 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:yocut/data/models/module.dart';
+
 import 'package:yocut/data/models/subject.dart';
 import 'package:yocut/ui/widgets/subjectFirstLetterContainer.dart';
 import 'package:yocut/utils/animationConfiguration.dart';
