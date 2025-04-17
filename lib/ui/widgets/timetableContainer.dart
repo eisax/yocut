00d242184051785476b21b8e529bc0feafbdfb4b
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-17 11:53:44 +0200

    feat:update commit, please review  commit 2025-04-17 11:53:44

--- lib/ui/widgets/timetableContainer.dart
+++ lib/ui/widgets/timetableContainer.dart
@@ -272,7 +272,7 @@ class _TimeTableContainerState extends State<TimeTableContainer>
                   height: imageHeight,
                   width: imageWidth,
                   radius: 7.5,
-                  subject: Module(moduleName:"", moduleId: "", moduleCode: "", moduleUnitCode: "", periodId: "", isEvaluable: "", pastExamPapers: P, vleStatus: true),
+                  subject: Module(moduleName:"", moduleId: "", moduleCode: "", moduleUnitCode: "", periodId: "", isEvaluable: "", pastExamPapers: PastExamPaper(pastExamPaperId: pastExamPaperId, year: year, description: description, documentSize: documentSize, periodId: periodId, documentPath: documentPath), vleStatus: true),
                 ),
           const SizedBox(
             width: 20,
